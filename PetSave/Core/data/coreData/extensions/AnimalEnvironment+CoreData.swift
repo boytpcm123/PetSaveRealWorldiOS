193bd02f189ccb789c8b6e5c3abfbbf4bb582e19
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2022-05-30 16:21:47 +0700

    Defining the Data Layer - Databases

--- PetSave/Core/data/coreData/extensions/AnimalEnvironment+CoreData.swift
+++ PetSave/Core/data/coreData/extensions/AnimalEnvironment+CoreData.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2022 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,38 +30,18 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import CoreData
 
-struct Animal: Codable {
-  var id: Int?
-  let organizationId: String?
-  let url: URL?
-  let type: String
-  let species: String?
-  var breeds: Breed
-  var colors: APIColors
-  let age: Age
-  let gender: Gender
-  let size: Size
-  let coat: Coat?
-  let name: String
-  let description: String?
-  let photos: [PhotoSizes]
-  let videos: [VideoLink]
-  let status: AdoptionStatus
-  var attributes: AnimalAttributes
-  var environment: AnimalEnvironment?
-  let tags: [String]
-  var contact: Contact
-  let publishedAt: String?
-  let distance: Double?
-  var ranking: Int? = 0
-
-  var picture: URL? {
-    photos.first?.medium ?? photos.first?.large
+// MARK: - CoreDataPersistable
+extension AnimalEnvironment: CoreDataPersistable {
+  var keyMap: [PartialKeyPath<AnimalEnvironment>: String] {
+    [
+      \.children: "children",
+      \.dogs: "dogs",
+      \.cats: "cats",
+      \.id: "id"
+    ]
   }
-}
 
-// MARK: - Identifiable
-extension Animal: Identifiable {
+  typealias ManagedType = AnimalEnvironmentEntity
 }
